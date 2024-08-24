04789cd5cc22b8c47feb54f31f2c78314eb71304
Alex V.B. <136813525+wi-p@users.noreply.github.com>
2024-08-23 21:36:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
-from kivy.uix.label import Label
 
 
 class MainScreen(Screen):
     def getValue(value): 
-        print(value)
-        
+        pass
+
 
 class BtnNumber(Button):
     def on_release(self):
         MainScreen.getValue(self.text)
-        LabelShow().text += self.text
-
-
-class LabelShow(Label):
-    def insertText(text):
-        pass
-
+        
 
 class CalculatorApp(App):
     def build(self):
